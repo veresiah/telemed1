e9304a1d351f67328610c070caa614f48cc893e1
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-07-18 19:57:56 -0400

    generate welcome controller and add route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get '/logut' => 'sessions#destroy'
   get 'signup' => 'patients#new'
 
-
+  root 'welcome#main'
+  
   resources :patients, except: [:index]
   resources :physicians 
   resources :appointments 
