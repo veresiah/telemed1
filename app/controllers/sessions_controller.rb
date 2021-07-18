0a63d2e0e248d432a810a7d8eae27ba549642204
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-07-18 14:34:32 -0400

    add session new create destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create 
+    patient = Patient.find_by(id: params[:id])
+    if patient && patient.aunthenticate(params[:password])
+      session[:user_id] = patient.id
+      redirect_to patient_path(patient)
+      flash[:success] = "Login was Successful!"
+    else 
+      flash[:error] = "Invalid Credentials, try again"      
+      render 'new'
+  end 
+
+  def destroy  
+    session.delete :user_id 
+    redirect_to login_path
+    flash[:notice] = "Logged out Successfully!"
+  end 
+
+
 end
