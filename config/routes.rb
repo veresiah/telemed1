182d26bff7e8d1412a19d779b58f9f6cedccc1d8
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-07-18 14:10:50 -0400

    add indepedent routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do 
-  
+  resources :patients, except: [:index]
+  resources :physicians 
+  resources :appointments 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
