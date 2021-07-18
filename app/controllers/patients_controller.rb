9a373007d24f030304364f353a7a589d87328058
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-07-18 13:25:44 -0400

    generate patients controller via generator

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,19 @@
+class PatientsController < ApplicationController
+  def new
+  end
+  
+  def create 
+  end 
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end 
+
+  def destroy 
+  end 
+end
