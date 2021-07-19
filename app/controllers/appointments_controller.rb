90f4befba03a0faca93bb10c0cb49b2f77d36e4f
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-07-19 16:51:17 -0400

    add before action, private methods > appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,5 @@
 class AppointmentsController < ApplicationController
+  before_action :set_appointment, except: [:new, :create]
   def new
     @appointment = Appointment.new 
   end
@@ -12,4 +13,20 @@ class AppointmentsController < ApplicationController
 
   def show
   end
+
+  def update 
+  end 
+
+  def destroy 
+  end 
+
+  private 
+  def appointment_params 
+    params.require(:appointment).permit(:date, :reason) 
+  end 
+
+  def set_appointment
+    @appointment = Appointment.find(params[:id])
+  end 
+
 end
