85098477cdbd4824aa80db76e8e098d114c7ebb3
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-07-18 20:19:02 -0400

    add from_omniauth method

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,17 @@
 class Patient < ApplicationRecord 
+    has_secure_password 
+    
     has_many :appointments 
     has_many :physicians, though: :appointments 
 
     validates :name, :password, presence: true
     validates :email, presence: true, uniqueness: true
+
+    def self.from_omniauth(response)
+        Patient.find_or_create_by(uid: response['uid'], provider: response['provider']) do |u|
+            u.name = response['info']['name']
+            u.email = response['info']['email']
+            u.password = SecureRandom.hex(15)
+        end
+    end 
 end 
