ec13f90e0491a50b4d084a0264eb0578ab8e9be2
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-07-19 13:20:59 -0400

    code out patients controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,8 +1,15 @@
 class PatientsController < ApplicationController
   def new
+    @patient =Patient.new
   end
   
   def create 
+    patient = Patient.new(patient_params)
+    if patient.save
+      redirect_to patient_path(patient)
+    else 
+      render 'new'
+    end 
   end 
 
   def edit
@@ -12,8 +19,21 @@ class PatientsController < ApplicationController
   end
 
   def update
+    if patient.update(patient_params)
+      redirect_to patient_path(patient)
+    else 
+      render 'edit'
+    end 
   end 
 
   def destroy 
+    @patient.destory 
+    redirect_to root_path
   end 
+
+  private 
+    
+    def patient_params
+        params.require(:patient).permit(:uid, :provider, :name, :password, :email, :age, :gender, :medical_conditions, :primary_care, :insurance)
+    end 
 end
