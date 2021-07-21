edc33d69e5dc32a3b8f510d8f181f44147aed280
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-07-21 07:56:37 -0400

    add scope methods for physicians

--- app/models/physician.rb
+++ app/models/physician.rb
@@ -3,4 +3,12 @@ class Physician < ApplicationRecord
     has_many :patients, through: :appointments 
 
     validates :name, :speciality, :hospital, :education, :years_in_practice, presence: true
+
+    def self.alphabetized
+        Physician.order(name: :asc)
+    end 
+
+    def self.sort_speciality
+        Physician.order(speciality: :asc)
+    end 
 end
