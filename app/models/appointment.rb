5350e055648de977fc3b278134f7a8ec52773201
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-07-21 08:04:13 -0400

    add date scope method to controller

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,4 +3,8 @@ class Appointment < ApplicationRecord
     belongs_to :physician
 
     validates :date, :reason, presence: true
+
+    def self.date
+        Appointment.order(date: :asc)
+    end 
 end
